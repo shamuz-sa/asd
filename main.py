6f00ab1554a0691e85fe9035fb66a6b39172155b
Succes ATCHEMI <succesatchemi8@gmail.com>
2024-03-07 08:03:17 +0000

    CORS configuration for frontend using

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from tasks import router as tasks_router
 from categories import router as categories_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(debug=True)
 
+ #Configuration CORS pour autoriser toutes les origines pendant le développement
+origins = [
+    "http://localhost",
+    "http://localhost:4200",  # Ajoutez l'URL de votre application front-end
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
